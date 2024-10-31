1c91dad0c30b52bdf0294da853fb5957760ace79
H-MW <142008733+H-MW@users.noreply.github.com>
2024-11-01 00:15:23 +0900

    コードの整理

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//import DOM
 const questionTitle = document.querySelector('#questionTitle')
 const answeredSumText = document.querySelectorAll('.answeredSumText')
 const questionSumText = document.querySelector('#questionSumText')
@@ -7,12 +8,23 @@ const submitButton = document.querySelector('#submitButton')
 const resultText = document.querySelector('#resultText')
 const nextButton = document.querySelector('#nextButton')
 
-function loadQuizTexts() {
-    answeredSumText[0].innerText = questionNum.toString();
-    answeredSumText[1].innerText = questionNum.toString();
+function quizReset() {
+    questionNum = 1; //問題数を初期状態にリセット
+
+    //quizOrderArrayを[0,1,2,3, ... quizDataLength - 1]に
+    for (let i = 0; i < quizDataLength; i++) {
+        quizOrderArray[i] = i;
+    }
+}
+
+// クイズを読み込む
+function quizDOM() {
+    id = quizOrderArray[questionNum -1] //読み込むべき問題のIDを取得
+
+    answeredSumText[0].innerText = answeredSumText[1].innerText = questionNum.toString();
     questionSumText.innerText = quizDataLength.toString();
-    document.title = '問題： ' + questionArray[numArray[id]];
-    questionTitle.innerText = questionArray[numArray[id]];
+    document.title = '問題： ' + questionArray[id];
+    questionTitle.innerText = questionArray[id];
     random.checked = isRandom;
     answerText.value = '';
     submitButton.disabled = '';
@@ -23,23 +35,23 @@ function loadQuizTexts() {
 async function fetchData() {
     //csvデータを読み込む
     const url = 'https://2s3wfjnsnup7qspt.public.blob.vercel-storage.com/data-quW8ABzgq7JnBkLDsafvIoo1WzPS0c.csv';
-    const response = await fetch(url).then((responses) => responses.text());
-    quizData = await csvToJson(response);
+    const response = await fetch(url).then((responses) => responses.text().catch((err) => {console.log(err)}));
+    quizData = csvToJson(response);
     
     quizDataLength = quizData.length;
 
     for (let i = 0; i < quizDataLength; i++) {
-        questionArray[i] = String(quizData[i].question);
-        correctAnswerArray[i] = String(quizData[i].correctAnswer);
-        correctAnswer2Array[i] = String(quizData[i].correctAnswer2);
+        questionArray[i] = String(quizData[i].question); //問題タイトルの配列作成
+        correctAnswerArray[i] = String(quizData[i].correctAnswer);  //正解の配列の作成
+        correctAnswer2Array[i] = String(quizData[i].correctAnswer2);  //正解2の配列の作成
     }
 }
 
-function csvToJson(data) {
+function csvToJson(csv) {
     //csvデータからjsonデータに変換
     let jsonArray = [];
 
-    let RowArray = data.split('\n');
+    let RowArray = csv.split('\n');
     let items = RowArray[0].split(',');
     for(let i = 1; i < RowArray.length; i++){
         let cellArray = RowArray[i].split(',');
@@ -52,11 +64,26 @@ function csvToJson(data) {
     return jsonArray;
 }
 
-function checkAnswer() {
-    let correctAnswer = correctAnswerArray[numArray[id]];
-    let correctAnswer2 = correctAnswer2Array[numArray[id]];
+function makeRandom(isRandom) {
+    if (isRandom) {
+        //quizOrderArray = [0,1,2,3, ... quizDataLength - 1] → [24,63,75, ...]のようにランダムに
+        for (let i = quizDataLength; i > 0; i--) {
+            let randNum = Math.floor(Math.random() * i);
+            let quizOrderArray_ = quizOrderArray[--i];
+            quizOrderArray[i] = quizOrderArray[randNum];
+            quizOrderArray[randNum] = quizOrderArray_;
+        }
+    }
+}
 
-    answerValue = answerText.value;
+function checkAnswer() {
+    submitButton.disabled = 'disabled';
+
+    const correctAnswer = correctAnswerArray[id];
+    const correctAnswer2 = correctAnswer2Array[id];
+    let correctAnswerText; //不正解時に表示するテキスト
+    
+    const answerValue = answerText.value; //回答を取得
 
     result = answerValue === correctAnswer || answerValue === correctAnswer2;
     
@@ -65,7 +92,7 @@ function checkAnswer() {
     }else {
         correctAnswerText = correctAnswer;
     }
-
+    
     if (result) {
         resultText.textContent = "正解です！";
         resultText.className = "result correct";
@@ -74,58 +101,40 @@ function checkAnswer() {
         resultText.className = "result incorrect";
     }
 
-    submitButton.disabled = 'disabled';
     resultText.style.display = 'block';
     nextButton.style.display = "block";
 }
 
-function nextQuestion() {
-    questionNum++;
-    id = numArray[questionNum - 1];
-    loadQuizTexts();
-}
+let questionNum; //経過問題数
+let id; //問題ID
+let questionArray = new Array(); //問題タイトルの配列
+let correctAnswerArray = new Array(); //正解の配列
+let correctAnswer2Array = new Array(); //正解2の配列(答えが複数ある時用)
+let quizOrderArray = new Array(); //問題の配列を呼び出す順番の配列
+let quizDataLength; //クイズデータの量を数値で代入
+let isRandom = false; //ランダム機能の有効/無効
 
-let id = 0;
-let questionArray = new Array();
-let correctAnswerArray = new Array();
-let correctAnswer2Array = new Array();
-let numArray = new Array();
-let quizDataLength;
-let questionNum = 1;
-let isRandom = false;
-let correctAnswerText;
 
-fetchData()
+fetchData() //クイズデータを取ってくる
 .then(() => {
-    for (let i = 0; i < quizDataLength; i++) {
-        numArray[i] = i;
-    }
-    loadQuizTexts();
+    //データの取得完了後の処理
+    quizReset()
+    quizDOM();
 })
 .catch((err) => {console.err(err);})
 
+//ランダム機能のチェックボックスの変更を検知
 random.addEventListener('change', () => {
-    isRandom = random.checked;
-    id = 0;
-    questionNum = 1;
-    
-    for (let i = 0; i < quizDataLength; i++) {
-        numArray[i] = i;
-    }
-
-    if (isRandom) {
-        for (let i = quizDataLength; i > 0; i--) {
-            let randNum = Math.floor(Math.random() * i);
-            let numArray_ = numArray[--i];
-            numArray[i] = numArray[randNum];
-            numArray[randNum] = numArray_;
-        }
-        loadQuizTexts();
-    }else {
-        loadQuizTexts();
-    }
+    isRandom = random.checked; //チェックの有無を確認
+    quizReset();
+    makeRandom(isRandom);
+    quizDOM();
     }
 )
 
 submitButton.addEventListener('click', checkAnswer);
-nextButton.addEventListener('click', nextQuestion);
+
+nextButton.addEventListener('click', () => {
+    questionNum++;
+    quizDOM();
+});
